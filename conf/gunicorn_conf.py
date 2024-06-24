79f47e16419609414656e48a799b96347407dfce
jacurlive <jasurkkirchh@gmail.com>
2024-06-24 16:18:55 +0500

    change gunicorn conf

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -1,4 +1,4 @@
-command = 'ecoaxl/gc_beta/venv/bin/gunicorn'
-pythonpath = 'ecoaxl/gc_beta'
+command = '/root/ecoaxl/gc_beta/venv/bin/gunicorn'
+pythonpath = '/root/ecoaxl/gc_beta'
 bind = '147.45.108.58:4200'
 workers = 3
