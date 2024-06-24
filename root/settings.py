273526667200e6e0d96352fcff706e1c7c6f5137
jacurlive <jasurkkirchh@gmail.com>
2024-06-24 15:33:29 +0500

    change settings

--- root/settings.py
+++ root/settings.py
@@ -27,12 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lvswi)ki!5983x7l8_k+$p8u_f79wza$-w&e1_ke7(_ckv5pf8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DEBUG'] == 'True'
+
+print(DEBUG)
 
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.ngrok-free.app"
+    "https://*.ngrok-free.app",
+    "https://*.ecoaxl.uz"
 ]
 
 
