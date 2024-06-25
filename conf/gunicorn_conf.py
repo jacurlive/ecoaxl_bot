eb7db77754f698b85a25a41b062a5cc543f9408c
jacurlive <jasurkkirchh@gmail.com>
2024-06-25 13:39:00 +0500

    fix for deploy

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -1,5 +1,3 @@
 bind = "0.0.0.0:8000"
 workers = 3
 loglevel = 'debug'
-errorlog = '/home/jacur/www-projects/GC-BETA/gc_beta/gunicorn_error.log'
-accesslog = '/home/jacur/www-projects/GC-BETA/gc_beta/gunicorn_access.log'
--- root/asgi.py
+++ root/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gc_beta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
 application = get_asgi_application()
